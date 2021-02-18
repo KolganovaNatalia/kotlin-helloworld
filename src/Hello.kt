0939052745b933065fd8772b6f994e5e1f880b1c
KolganovaNatalia <kolganova.natalia@gmail.com>
2021-02-18 15:29:28 +0300

    task 3 is completed

--- src/Hello.kt
+++ src/Hello.kt
@@ -1,5 +1,10 @@
 fun main(args: Array<String>){
-    val hello: String = "hello"
-    val aplana: String = "aplana"
-    println("$hello $aplana")
+    var five: Char = '5'
+    println("5 to string: " + (5.toString()))
+    println("5 to byte: " + (5.toByte()))
+    println("5 to short: " + (5.toShort()))
+    println("5 to int: " + (5.toInt()))
+    println("5 to double: " + (5.toDouble()))
+    println("5 to float: " + (5.toFloat()))
+    println("5 to long: " + (5.toLong()))
 }
