3a8f70b3fc079c32cb6a9c64aa8faf274812c408
KolganovaNatalia <kolganova.natalia@gmail.com>
2021-02-18 16:51:59 +0300

    task 4 is completed

--- src/Hello.kt
+++ src/Hello.kt
@@ -1,10 +1,6 @@
 fun main(args: Array<String>){
-    var five: Char = '5'
-    println("5 to string: " + (5.toString()))
-    println("5 to byte: " + (5.toByte()))
-    println("5 to short: " + (5.toShort()))
-    println("5 to int: " + (5.toInt()))
-    println("5 to double: " + (5.toDouble()))
-    println("5 to float: " + (5.toFloat()))
-    println("5 to long: " + (5.toLong()))
+    println("Введите имя и нажмите Enter:")
+    var userText: String? = readLine()
+    var stringText: String = userText.toString()
+    print("Приветствую, $stringText")
 }
