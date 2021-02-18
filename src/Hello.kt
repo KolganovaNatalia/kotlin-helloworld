51b994c0d16a30cb3641cdb2f0552a0ab1d3e269
KolganovaNatalia <kolganova.natalia@gmail.com>
2021-02-18 14:48:35 +0300

    task 2 is completed

--- src/Hello.kt
+++ src/Hello.kt
@@ -1,6 +1,5 @@
 fun main(args: Array<String>){
-    var myvar: Any? = null
-    myvar = 5
-    var myvar1 = myvar / 2.5 // float type
-    println("Hello Kotlin")
+    val hello: String = "hello"
+    val aplana: String = "aplana"
+    println("$hello $aplana")
 }
