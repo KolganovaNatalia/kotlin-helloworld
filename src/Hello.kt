3be0deb6488050fa2aa69a5ded75fcbe0f59285f
KolganovaNatalia <kolganova.natalia@gmail.com>
2021-02-20 20:38:45 +0300

    task 5 is completed

--- src/Hello.kt
+++ src/Hello.kt
@@ -1,6 +1,4 @@
 fun main(args: Array<String>){
-    println("Введите имя и нажмите Enter:")
-    var userText: String? = readLine()
-    var stringText: String = userText.toString()
-    print("Приветствую, $stringText")
+   val range = 'z' downTo 'a'
+   println(range)
 }
