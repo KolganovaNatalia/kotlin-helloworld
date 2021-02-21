ab2abd49ef850cece39a1809f923d8adb7b71db0
KolganovaNatalia <kolganova.natalia@gmail.com>
2021-02-21 18:23:12 +0300

    task 6 is completed

--- src/Hello.kt
+++ src/Hello.kt
@@ -1,4 +1,9 @@
 fun main(args: Array<String>){
-   val range = 'z' downTo 'a'
-   println(range)
+   println("Введите число a и нажмите Enter")
+   var a = readLine()!!.toInt()
+   println("Введите число b и нажмите Enter")
+   var b = readLine()!!.toInt()
+   println("a=$a b=$b")
+   println("$a" > "$b")
+
 }
